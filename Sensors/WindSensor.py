5c1fe667230c0ed84b3798af370ed312c2bddf4b
Simon Sarcevic <sarcevicsimon07+main@gmail.com>
2025-12-11 11:52:38 +0100

    Update WindSensor.py

--- Sensors/WindSensor.py
+++ Sensors/WindSensor.py
@@ -1,11 +1,17 @@
 from abc import ABC
+from http.client import responses
+import requests
 
 from Sensors.Sensor import Sensor
 from interface.Source import Source
 
 class WindSensor(Sensor, Source, ABC):
-    def __init__(self, name, unit, value):
-        super().__init__(name, unit, value)
+    def __init__(self):
+        self.url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=relative_humidity_2m,temperature_2m,wind_speed_10m"
 
     def show(self):
-        print(f"Wind Sensor: {Sensor.getvalue(self)}")
+        responses = requests.get(self.url)
+        data = responses.json()
+        wind_speed = data['hourly']['wind_speed_10m'][0]
+
+        print(f'Wind Speed: {wind_speed}km/h')
