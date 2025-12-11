38fe01778cfeb94972109f114c2dd58c4f0b4025
Simon Sarcevic <sarcevicsimon07+main@gmail.com>
2025-12-11 11:51:04 +0100

    Update TempSensor.py

--- Sensors/TempSensor.py
+++ Sensors/TempSensor.py
@@ -1,11 +1,16 @@
 from abc import ABC
+import requests
 
 from Sensors.Sensor import Sensor
 from interface.Source import Source
 
 class TempSensor(Sensor, Source, ABC):
-    def __init__(self, name, unit, value):
-        super().__init__(name, unit, value)
+    def __init__(self):
+        self.url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=relative_humidity_2m,temperature_2m,wind_speed_10m"
 
     def show(self):
-        print(f"Temperature: {Sensor.getvalue(self)}")
+        responses = requests.get(self.url)
+        data = responses.json()
+        temp = data['hourly']['temperature_2m'][0]
+
+        print(f'Temperature: {temp}°C')
